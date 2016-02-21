9eff812c60b87bec00b4a745162a6238ca4e71f5
Nathan Ross <nross.se@gmail.com>
2016-02-21 15:43:00 -0500

    appveyor testing

--- tooling/build/windows/appveyor.common.ps1
+++ tooling/build/windows/appveyor.common.ps1
@@ -1,12 +1,13 @@
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 
-$env:MBASH =
 $env:MSYS2_BASEVER = "20160205"
 $env:MSYS2_ARCH = "x86_64"
-$env:MSYS2_DIR="${env:APPVEYOR_BUILD_FOLDER}\tmp\msys2"
-$env:RUST_DIR="${env:APPVEYOR_BUILD_FOLDER}\tmp\rust"
+$env:REPO_TEMP_DIR= "${env:APPVEYOR_BUILD_FOLDER}\tmp"
+$env:MSYS2_DIR = "${env:REPO_TEMP_DIR}\msys2"
+$env:RUST_DIR= "${env:REPO_TEMP_DIR}\rust"
 $env:PATH="${env:PATH}:${env:RUST_DIR}\bin:${env:MSYS2_DIR}\bin"
 
-mbash($command) {
-    $MSYS2_DIR\msys64\usr\bin\sh --login -c "cd $APPVEYOR_BUILD_FOLDER; PATH=$PATH:/mingw64/bin:/mingw32/bin; exec 0</dev/null; $command"
+Function mbash($command) {
+    echo "mbash"
+#    ${env:MSYS2_DIR}\msys64\usr\bin\./sh --login -c "cd ${env:APPVEYOR_BUILD_FOLDER}; PATH=${PATH}:/mingw64/bin:/mingw32/bin; exec 0</dev/null; $command"
 }
--- tooling/build/windows/appveyor.install.ps1
+++ tooling/build/windows/appveyor.install.ps1
@@ -1,21 +1,28 @@
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
-. $scriptPath\common.ps1
+. $scriptPath\appveyor.common.ps1
 
-Function install_msys2($install_dir, $arch, $ver) {
-    $MBASH ="msys64\usr\bin\sh --login -c"
-    echo Start-FileDownload "https://kent.dl.sourceforge.net/project/msys2/Base/${arch}/msys2-base-${arch}-${ver}.tar.xz" -FileName "msys2.tar.xz"
-    Start-FileDownload "https://kent.dl.sourceforge.net/project/msys2/Base/${arch}/msys2-base-${arch}-${ver}.tar.xz" -FileName "msys2.tar.xz"
-    7z x msys2.tar.xz
-    7Z x msys2.tar > NUL
-    $mbash="msys64\usr\bin\sh --login -c"
-#    $mbash ""
-#    $mbash "for i in {1..3}; do pacman --noconfirm -Suy mingw-w64-%MSYS2_ARCH%-{ragel,freetype,icu,gettext} libtool pkg-config gcc make autoconf automake perl && break || sleep 15; done"
+Function install_msys2($temp_dir, $install_dir, $arch, $ver) {
+    #$download_loc ="https://kent.dl.sourceforge.net/project/msys2/Base/${arch}/msys2-base-${arch}-${ver}.tar.xz"
+    $download_loc = "http://downloads.sourceforge.net/project/msys2/Base/${arch}/msys2-base-${arch}-${ver}.tar.xz?r=&ts=1456082848&use_mirror=iweb"
+    echo "downloading from $download_loc"    
+    (New-Object System.Net.WebClient).DownloadFile($download_loc, "${temp_dir}\msys2.tar.xz")
+    7z x "${temp_dir}\msys2.tar.xz" -o"${temp_dir}"
+    7Z x "${temp_dir}\msys2.tar" -o"${temp_dir}" | Out-Null
+    mbash("")
+    mbash("for i in {1..3}; do pacman --noconfirm -Suy mingw-w64-%MSYS2_ARCH%-{ragel,freetype,icu,gettext} libtool pkg-config gcc make autoconf automake perl && break || sleep 15; done")
 }
 
-Function install_rust($install_dir, $target_rs_triple, $rustc_ver) {
-    Start-FileDownload "https://static.rust-lang.org/dist/rust-${env:rustc_ver}-${target_rs_triple}.exe" -FileName rust.exe
-    rust.exe /VERYSILENT /NORESTART /DIR="$install_dir"
+Function install_rust($temp_dir, $install_dir, $target_rs_triple, $rustc_ver) {
+    $download_loc = "https://static.rust-lang.org/dist/rust-${rustc_ver}-${target_rs_triple}.exe"
+    echo "downloading from $download_loc"
+    (New-Object System.Net.WebClient).DownloadFile($download_loc, "${temp_dir}/rust.exe")
+#    ${temp_dir}/./rust.exe /VERYSILENT /NORESTART /DIR="$install_dir"
 }
 
-install_msys2(${env:MSYS2_DIR}, ${env:MSYS2_ARCH}, ${env:MSYS2_BASEVER})
-install_rust(${env:RUST_DIR}, ${env:TARGET}, ${env:RUSTC_V})
+Function mktmpdir($tmpdir_path) {
+    New-Item -ItemType Directory -Force -Path $tmpdir_path
+}
+
+mktmpdir ${env:REPO_TEMP_DIR}
+install_msys2 ${env:REPO_TEMP_DIR} ${env:MSYS2_DIR} ${env:MSYS2_ARCH} ${env:MSYS2_BASEVER}
+install_rust ${env:REPO_TEMP_DIR} ${env:RUST_DIR} ${env:TARGET} ${env:RUSTC_V}
