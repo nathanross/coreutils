bd59e75516eb34e8b0f77bb9fe72f997d15ace06
Nathan Ross <nross.se@gmail.com>
2016-02-22 23:38:33 -0500

    test appveyor native msys2

--- tooling/build/windows/appveyor.install.ps1
+++ tooling/build/windows/appveyor.install.ps1
@@ -1,5 +1,36 @@
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
-. $scriptPath\appveyor.common.ps1
+#. $scriptPath\appveyor.common.ps1
+
+$env:MSYS2_BASEVER = "20160205"
+$env:MSYS2_ARCH = "x86_64"
+$env:DIR_TEMP= "c:\temp"
+$env:DIR_BUILD_CACHE= "c:\cache"
+$env:DIR_MSYS2_DOWNLOAD= "${env:DIR_TEMP}"
+$env:DIR_RUST_DOWNLOAD= "${env:DIR_BUILD_CACHE}"
+$env:DIR_RUST_INSTALL= "${env:DIR_TEMP}\rust"
+
+if (${env:TARGET}.endsWith("gnu")) {
+   $env:DIR_MSYS2_INSTALL="${env:APPVEYOR_BUILD_FOLDER}\msys64"
+} else {
+   $env:DIR_MSYS2_INSTALL="c:\msys64"      
+}
+
+$env:MINGW_DIR_USED = "${env:APPVEYOR_BUILD_FOLDER}"
+
+$env:PATH="${env:PATH}:${env:RUST_INSTALL}\bin:${env:DIR_MINGW_INSTALL}\bin:${env:DIR_MINGW_INSTALL}\usr\bin"
+
+Function unixify($winpath) {
+    return $winpath -replace 'c:','/c' -replace '\\','/'
+}
+
+Function mbash($command) {
+    echo "mingw call: ${command}"
+    cd "${env:DIR_MSYS2_INSTALL}\usr\bin\"
+    $unixpath_appveyor_build_folder = unixify ${env:APPVEYOR_BUILD_FOLDER}
+    $unixpath_dir_rust_install = unixify ${env:DIR_RUST_INSTALL}
+    $unixpath_msys2_install = unixify ${env:DIR_MSYS2_INSTALL}
+    .\"sh" --login -c "cd ${unixpath_appveyor_build_folder}; export PATH=`$PATH:${unixpath_dir_rust_install}/bin:${unixpath_msys2_install}/mingw32/bin:${unixpath_msys2_install}/mingw64/bin; exec 0</dev/null; $command; exit $?"
+}
 
 Function install_msys2($download_dir, $arch, $ver) {
     #$download_loc ="https://kent.dl.sourceforge.net/project/msys2/Base/${arch}/msys2-base-${arch}-${ver}.tar.xz"
