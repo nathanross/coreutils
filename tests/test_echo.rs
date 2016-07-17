f17c5e41ce6fd1afadf394cc689a54981c781e6a
Nathan Ross <nross.se@gmail.com>
2016-07-17 14:16:15 -0400

    tests: echo refactor to be more terse through assert composition

--- tests/test_echo.rs
+++ tests/test_echo.rs
@@ -5,32 +5,31 @@ static UTIL_NAME: &'static str = "echo";
 #[test]
 fn test_default() {
     let (_, mut ucmd) = testing(UTIL_NAME);
-    assert_eq!(ucmd.run().stdout, "\n");
+    ucmd.run()
+        .succeeds()
+        .stdout_only("\n");
 }
 
 #[test]
 fn test_no_trailing_newline() {
     let (_, mut ucmd) = testing(UTIL_NAME);
-    ucmd.arg("-n")
-        .arg("hello_world");
-
-    assert_eq!(ucmd.run().stdout, "hello_world");
+    ucmd.arg("-n").arg("hello_world")
+        .succeeds()
+        .stdout_only("hello_world");
 }
 
 #[test]
 fn test_enable_escapes() {
     let (_, mut ucmd) = testing(UTIL_NAME);
-    ucmd.arg("-e")
-        .arg("\\\\\\t\\r");
-
-    assert_eq!(ucmd.run().stdout, "\\\t\r\n");
+    ucmd.arg("-e").arg("\\\\\\t\\r")
+        .succeeds()
+        .stdout_only("\\\t\r\n");
 }
 
 #[test]
 fn test_disable_escapes() {
     let (_, mut ucmd) = testing(UTIL_NAME);
-    ucmd.arg("-E")
-        .arg("\\b\\c\\e");
-
-    assert_eq!(ucmd.run().stdout, "\\b\\c\\e\n");
+    ucmd.arg("-E").arg("\\b\\c\\e")
+        .succeeds()
+        .stdout_only("\\b\\c\\e\n");
 }
