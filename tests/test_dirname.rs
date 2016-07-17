c9b43f314b92204f1ea7cd0bf6d4880a1244b732
Nathan Ross <nross.se@gmail.com>
2016-07-17 14:06:24 -0400

    tests: dirname refactor to be more terse through assert composition

--- tests/test_dirname.rs
+++ tests/test_dirname.rs
@@ -5,44 +5,34 @@ static UTIL_NAME: &'static str = "dirname";
 #[test]
 fn test_path_with_trailing_slashes() {
     let (_, mut ucmd) = testing(UTIL_NAME);
-    let dir = "/root/alpha/beta/gamma/delta/epsilon/omega//";
-    let out = ucmd.arg(dir).run().stdout;
-
-    assert_eq!(out.trim_right(), "/root/alpha/beta/gamma/delta/epsilon");
+    ucmd.arg("/root/alpha/beta/gamma/delta/epsilon/omega//").succeeds()
+        .stdout_only("/root/alpha/beta/gamma/delta/epsilon");
 }
 
 #[test]
 fn test_path_without_trailing_slashes() {
     let (_, mut ucmd) = testing(UTIL_NAME);
-    let dir = "/root/alpha/beta/gamma/delta/epsilon/omega";
-    let out = ucmd.arg(dir).run().stdout;
-
-    assert_eq!(out.trim_right(), "/root/alpha/beta/gamma/delta/epsilon");
+    ucmd.arg("/root/alpha/beta/gamma/delta/epsilon/omega").succeeds()
+        .stdout_only("/root/alpha/beta/gamma/delta/epsilon");
 }
 
 #[test]
 fn test_root() {
     let (_, mut ucmd) = testing(UTIL_NAME);
-    let dir = "/";
-    let out = ucmd.arg(dir).run().stdout;
-
-    assert_eq!(out.trim_right(), "/");
+    ucmd.arg("/").succeeds()
+        .stdout_only("/");
 }
 
 #[test]
 fn test_pwd() {
     let (_, mut ucmd) = testing(UTIL_NAME);
-    let dir = ".";
-    let out = ucmd.arg(dir).run().stdout;
-
-    assert_eq!(out.trim_right(), ".");
+    ucmd.arg(".").succeeds()
+        .stdout_only(".");
 }
 
 #[test]
 fn test_empty() {
     let (_, mut ucmd) = testing(UTIL_NAME);
-    let dir = "";
-    let out = ucmd.arg(dir).run().stdout;
-
-    assert_eq!(out.trim_right(), ".");
+    ucmd.arg("").succeeds()
+        .stdout_only(".");
 }
