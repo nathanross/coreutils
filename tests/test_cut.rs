313253beb0d4220a5f586e3cdd7f13a8e1d9b519
Nathan Ross <nross.se@gmail.com>
2016-07-17 14:12:07 -0400

    tests: cut refactor to be more terse through assert composition

--- tests/test_cut.rs
+++ tests/test_cut.rs
@@ -8,50 +8,49 @@ static INPUT: &'static str = "lists.txt";
 #[test]
 fn test_prefix() {
     let (at, mut ucmd) = testing(UTIL_NAME);
-    let result = ucmd.args(&["-c", "-10", INPUT]).run();
-    assert_eq!(result.stdout, at.read("lists_prefix.expected"));
+    ucmd.args(&["-c", "-10", INPUT]).succeeds()
+        .stdout_only(at.read("lists_prefix.expected"));
 }
 
 #[test]
 fn test_char_range() {
     let (at, mut ucmd) = testing(UTIL_NAME);
-    let result = ucmd.args(&["-c", "4-10", INPUT]).run();
-    assert_eq!(result.stdout, at.read("lists_char_range.expected"));
+    ucmd.args(&["-c", "4-10", INPUT]).succeeds()
+        .stdout_only(at.read("lists_char_range.expected"));
 }
 
 #[test]
 fn test_column_to_end_of_line() {
     let (at, mut ucmd) = testing(UTIL_NAME);
-    let result = ucmd.args(&["-d", ":", "-f", "5-", INPUT]).run();
-    assert_eq!(result.stdout,
-               at.read("lists_column_to_end_of_line.expected"));
+    ucmd.args(&["-d", ":", "-f", "5-", INPUT]).succeeds()
+        .stdout_only(at.read("lists_column_to_end_of_line.expected"));
 }
 
 #[test]
 fn test_specific_field() {
     let (at, mut ucmd) = testing(UTIL_NAME);
-    let result = ucmd.args(&["-d", " ", "-f", "3", INPUT]).run();
-    assert_eq!(result.stdout, at.read("lists_specific_field.expected"));
+    ucmd.args(&["-d", " ", "-f", "3", INPUT]).succeeds()
+        .stdout_only(at.read("lists_specific_field.expected"));
 }
 
 #[test]
 fn test_multiple_fields() {
     let (at, mut ucmd) = testing(UTIL_NAME);
-    let result = ucmd.args(&["-d", ":", "-f", "1,3", INPUT]).run();
-    assert_eq!(result.stdout, at.read("lists_multiple_fields.expected"));
+    ucmd.args(&["-d", ":", "-f", "1,3", INPUT]).succeeds()
+        .stdout_only(at.read("lists_multiple_fields.expected"));
 }
 
 #[test]
 fn test_tail() {
     let (at, mut ucmd) = testing(UTIL_NAME);
-    let result = ucmd.args(&["-d", ":", "--complement", "-f", "1", INPUT]).run();
-    assert_eq!(result.stdout, at.read("lists_tail.expected"));
+    ucmd.args(&["-d", ":", "--complement", "-f", "1", INPUT]).succeeds()
+        .stdout_only(at.read("lists_tail.expected"));
 }
 
 #[test]
 fn test_change_delimiter() {
     let (at, mut ucmd) = testing(UTIL_NAME);
-    let result = ucmd.args(&["-d", ":", "--complement", "--output-delimiter=#", "-f", "1", INPUT])
-                     .run();
-    assert_eq!(result.stdout, at.read("lists_change_delimiter.expected"));
+    ucmd.args(&["-d", ":", "--complement", "--output-delimiter=#", "-f", "1", INPUT])
+        .succeeds()
+        .stdout_only(at.read("lists_change_delimiter.expected"));
 }
