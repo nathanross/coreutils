98984b7351b9d5e244d5c28a50f6a999fce0355c
Nathan Ross <nross.se@gmail.com>
2016-07-18 07:15:55 -0400

    tests: comm incomplete refactor for readability through assert composition

--- tests/test_comm.rs
+++ tests/test_comm.rs
@@ -3,68 +3,62 @@ use std::ffi::OsStr;
 
 static UTIL_NAME: &'static str = "comm";
 
-fn comm<A: AsRef<OsStr>, B: AsRef<str>>(args: &[A],
-                                                       file_stdout_relpath_opt: Option<B>,
-                                                       error_message_opt: Option<B>) {
-    let (at, mut ucmd) = testing(UTIL_NAME);
-    let result = ucmd.args(args)
-                     .run();
-    assert!(result.success == error_message_opt.is_none());
-    if let Some(_) = error_message_opt {
-        assert!(result.stderr.len() > 0);
-        // assert!(result.stderr.trim_right() == s);
-    } else {
-        assert!(result.stderr.len() == 0);
-    }
-    if let Some(file_stdout_relpath) = file_stdout_relpath_opt {
-        assert!(result.stdout == at.read(file_stdout_relpath.as_ref()))
-    }
-}
-
 #[test]
 fn ab_no_args() {
-    comm(&["a", "b"], Some("ab.expected"), None);
+    let (at, mut ucmd) = testing(UTIL_NAME);
+    ucmd.args(&["a", "b"]).succeeds()
+        .stdout_only(at.read("ab.expected")));
 }
 
 #[test]
 fn ab_dash_one() {
-    comm(&["a", "b", "-1"], Some("ab1.expected"), None);
+    let (at, mut ucmd) = testing(UTIL_NAME);
+    ucmd.args(&["a", "b", "-1"]).succeeds()
+        .stdout_only(at.read("ab.expected")));
 }
 
 #[test]
 fn ab_dash_two() {
-    comm(&["a", "b", "-2"], Some("ab2.expected"), None);
+    let (at, mut ucmd) = testing(UTIL_NAME);
+    ucmd.args(&["a", "b", "-2"]).succeeds()
+        .stdout_only(at.read("ab2.expected")));
 }
 
 #[test]
 fn ab_dash_three() {
-    comm(&["a", "b", "-3"], Some("ab3.expected"), None);
+    let (at, mut ucmd) = testing(UTIL_NAME);
+    ucmd.args(&["a", "b", "-3"]).succeeds()
+        .stdout_only(at.read("ab3.expected")));
 }
 
 #[test]
 fn aempty() {
-    comm(&["a", "empty"], Some("aempty.expected"), None);
+    let (at, mut ucmd) = testing(UTIL_NAME);
+    ucmd.args(&["a", "empty"]).succeeds()
+        .stdout_only(at.read("aempty.expected")));
 }
 
 #[test]
 fn emptyempty() {
-    comm(&["empty", "empty"], Some("emptyempty.expected"), None);
+    let (at, mut ucmd) = testing(UTIL_NAME);
+    ucmd.args(&["empty", "empty"]).succeeds()
+        .stdout_only(at.read("aempty.expected")));
 }
 
 #[cfg_attr(not(feature="test_unimplemented"),ignore)]
 #[test]
 fn output_delimiter() {
-    comm(&["--output-delimiter=word", "a", "b"],
-               Some("ab_delimiter_word.expected"),
-               None);
+    let (at, mut ucmd) = testing(UTIL_NAME);
+    ucmd.args(&["--output-delimiter=word", "a", "b"]).succeeds()
+        .stdout_only(at.read("ab_delimiter_word.expected"));
 }
 
 #[cfg_attr(not(feature="test_unimplemented"),ignore)]
 #[test]
 fn output_delimiter_require_arg() {
-    comm(&["--output-delimiter=", "a", "b"],
-               None,
-               Some("error to be defined"));
+    let (_, mut ucmd) = testing(UTIL_NAME);
+    assert!(ucmd.args(&["--output-delimiter=", "a", "b"])
+        .fails().no_stdout().stderr.len() > 0);
 }
 
 // even though (info) documentation suggests this is an option
@@ -74,24 +68,26 @@ fn output_delimiter_require_arg() {
 #[test]
 fn zero_terminated() {
     for param in vec!["-z", "--zero-terminated"] {
-        comm(&[param, "a", "b"], None, Some("error to be defined"));
+        let (_, mut ucmd) = testing(UTIL_NAME);
+        assert!(ucmd.args(&[param, "a", "b"])
+                .fails().no_stdout().stderr.len() > 0);
     }
 }
 
 #[cfg_attr(not(feature="test_unimplemented"),ignore)]
 #[test]
 fn check_order() {
-    comm(&["--check-order", "bad_order_1", "bad_order_2"],
-               Some("bad_order12.check_order.expected"),
-               Some("error to be defined"));
+    assert!(
+        ucmd.args(&["--check-order", "bad_order_1", "bad_order_2"]).fails()
+            .stdout_is("bad_order12.check_order.expected")
+            .stderr.len() > 0)
 }
 
 #[cfg_attr(not(feature="test_unimplemented"),ignore)]
 #[test]
 fn nocheck_order() {
-    comm(&["--nocheck-order", "bad_order_1", "bad_order_2"],
-               Some("bad_order12.nocheck_order.expected"),
-               None);
+    ucmd.args(&["--nocheck-order", "bad_order_1", "bad_order_2"]).succeeds()
+        .stdout_only(at.read("bad_order12.nocheck_order.expected"));
 }
 
 // when neither --check-order nor --no-check-order is provided,
@@ -147,18 +143,12 @@ fn unintuitive_default_behavior_1() {
 #[test]
 fn no_arguments() {
     let (_, mut ucmd) = testing(UTIL_NAME);
-    let result = ucmd.run();
-    assert!(!result.success);
-    assert!(result.stdout.len() == 0);
-    assert!(result.stderr.len() > 0);
+    assert!(ucmd.fails().no_stdout().stderr.len() > 0);
 }
 
 #[ignore] //bug? should help be stdout if not called via -h|--help?
 #[test]
 fn one_argument() {
     let (_, mut ucmd) = testing(UTIL_NAME);
-    let result = ucmd.arg("a").run();
-    assert!(!result.success);
-    assert!(result.stdout.len() == 0);
-    assert!(result.stderr.len() > 0);
+    assert!(ucmd.arg("a").fails().no_stdout().stderr.len() > 0);
 }
